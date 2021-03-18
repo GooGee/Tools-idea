37425f0b649e94974ad4184fd129c971476fc728
GitHub Action <action@github.com>
2021-03-18 03:46:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/googee/tools/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/googee/tools/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.googee.tools.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.googee.tools.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/googee/tools/services/MyProjectService.kt
+++ src/main/kotlin/com/github/googee/tools/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.googee.tools.services
 
+import com.github.googee.tools.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
